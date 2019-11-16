5ac61f9a9130ca142fc9b0f13c8fc6be2ebb00e7
LuisBarufi <luisbarufi@gmail.com>
2019-11-15 21:05:29 -0300

    updated

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 
+import './styles.css';
 
-function App() {
-  return (
+import Header from './components/Header';
+
+const App = () => (
     <div className="App">
-      <h1>Hello Rocketseat</h1>
+      <Header />
     </div>
   );
-}
+
 
 export default App;
