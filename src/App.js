af0030687264abf82486d8edf5f3d0cc232ecf15
LuisBarufi <luisbarufi@gmail.com>
2019-11-15 22:11:58 -0300

    updated

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import React from 'react';
 import './styles.css';
 
 import Header from './components/Header';
+import Main from './pages/main';
 
 const App = () => (
-    <div className="App">
-      <Header />
-    </div>
-  );
+  <div className="App">
+    <Header />
+    <Main />
+  </div>
+);
 
 
 export default App;
